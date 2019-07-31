8f37d503dcfd61d88d7b9b90324009049947e629
yuvaraniyuvarajan <53534281+yuvaraniyuvarajan@users.noreply.github.com>
2019-07-31 22:17:06 +0530

    Create +ve or-ve or 0.c

---
+++ +ve or-ve or 0.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+{
+printf("POSITIVE");
+}
+else if(n<0)
+{
+printf("NEGATIVE");
+}
+else
+{
+printf("ZERO");
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
